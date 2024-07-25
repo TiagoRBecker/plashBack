d26954911c35ba6386231a4e9d4fb1004db18473
Tiago Becker <beckertiago09@gmail.com>
2024-07-25 12:51:01 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 export const io =  new Server(server,{
   cors:{
-   origin:["http://localhost:3000","http://localhost:3001","https://ca2a-177-22-167-216.ngrok-free.app"],
+   origin:["http://localhost:3000","http://localhost:3001","http://77.37.69.19:3000"],
    methods:["GET","POST"]
   }
 });
