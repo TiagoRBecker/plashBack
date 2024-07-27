b52b967b4b76c5735de827cdb4cacd038602b4ed
Tiago Becker <84158213+TiagoRBecker@users.noreply.github.com>
2024-07-27 13:57:33 -0300

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import AuthControllers from "../Controllers/Auth";
 const router = Router();
 router.post("/signup/admin", AuthControllers.createAccountUserMaster);
 router.post("/signin/admin", AuthControllers.authenticationAdmin);
+router.post("/signin/employee", AuthControllers.authenticationEmployee);
 router.post("/signup", AuthControllers.createAccount);
 router.post("/signIn", AuthControllers.authentication);
 export default router;
